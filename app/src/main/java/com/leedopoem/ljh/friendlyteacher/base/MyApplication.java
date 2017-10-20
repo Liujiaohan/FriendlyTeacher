14814ed976640ce1170aa39bfd83df78dff161c2
enble du <384509085@qq.com>
2017-10-20 23:51:05 +0800

    修改MyApplication，可全局获取contxt

--- app/src/main/java/com/leedopoem/ljh/friendlyteacher/base/MyApplication.java
+++ app/src/main/java/com/leedopoem/ljh/friendlyteacher/base/MyApplication.java
@@ -1,6 +1,7 @@
 package com.leedopoem.ljh.friendlyteacher.base;
 
 import android.app.Application;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import io.realm.Realm;
@@ -12,6 +13,7 @@ import io.realm.RealmConfiguration;
  */
 
 public class MyApplication extends Application {
+    private static Context context;
     public static MyApplication INSTANCE=null;
     private SQLiteDatabase mDB;
 
@@ -19,10 +21,14 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         INSTANCE=this;
+        context = getApplicationContext();
         RealmConfiguration configuration=new RealmConfiguration.Builder(this).build();
         Realm.setDefaultConfiguration(configuration);
     }
 
+    public static Context getContext() {
+        return context;
+    }
 
     public static MyApplication getINSTANCE() {
         return INSTANCE;
