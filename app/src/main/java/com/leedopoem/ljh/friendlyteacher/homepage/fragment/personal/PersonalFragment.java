e57fd55eb190dd703ab550e04fee8a22d00c8491
enble du <384509085@qq.com>
2017-11-02 20:16:30 +0800

    解决bug

--- app/src/main/java/com/leedopoem/ljh/friendlyteacher/homepage/fragment/personal/PersonalFragment.java
+++ app/src/main/java/com/leedopoem/ljh/friendlyteacher/homepage/fragment/personal/PersonalFragment.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.leedopoem.ljh.friendlyteacher.R;
@@ -39,9 +39,9 @@ public class PersonalFragment extends Fragment implements PersonalConstract.View
     private TextView StudingLectureNumText;
     private TextView CollectedLectureNumText;
     private Button SettingBtn;
-    private LinearLayout PublishedLectureBtn;
-    private LinearLayout StudingLectureBtn;
-    private LinearLayout CollectedLectureBtn;
+    private RelativeLayout PublishedLectureBtn;
+    private RelativeLayout StudingLectureBtn;
+    private RelativeLayout CollectedLectureBtn;
     private CardView AboutusBtn;
     private Context context;
 
