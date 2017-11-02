875e30570384a4de2fa88b8fd791ab24cfdd4c1a
enble du <384509085@qq.com>
2017-11-02 20:11:35 +0800

    增加点击事件

--- app/src/main/java/com/leedopoem/ljh/friendlyteacher/homepage/fragment/personal/PersonalFragment.java
+++ app/src/main/java/com/leedopoem/ljh/friendlyteacher/homepage/fragment/personal/PersonalFragment.java
@@ -1,14 +1,20 @@
 package com.leedopoem.ljh.friendlyteacher.homepage.fragment.personal;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.leedopoem.ljh.friendlyteacher.R;
 import com.leedopoem.ljh.friendlyteacher.data.entity.PersonalInfo;
+import com.leedopoem.ljh.friendlyteacher.personaldetailmessage.DetailMessageActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,20 +24,26 @@ import com.leedopoem.ljh.friendlyteacher.data.entity.PersonalInfo;
  * create an instance of this fragment.
  */
 
-public class PersonalFragment extends Fragment implements PersonalConstract.View {
+public class PersonalFragment extends Fragment implements PersonalConstract.View ,View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     private PersonalConstract.Presenter personalPresenter;
-    TextView UserNameText;
-    TextView IntroductionText;
-    TextView NoticedNumText;
-    TextView FansNumText;
-    TextView PublishedLectureNumText;
-    TextView StudingLectureNumText;
-    TextView CollectedLectureNumText;
+    private TextView UserNameText;
+    private TextView IntroductionText;
+    private TextView NoticedNumText;
+    private TextView FansNumText;
+    private TextView PublishedLectureNumText;
+    private TextView StudingLectureNumText;
+    private TextView CollectedLectureNumText;
+    private Button SettingBtn;
+    private LinearLayout PublishedLectureBtn;
+    private LinearLayout StudingLectureBtn;
+    private LinearLayout CollectedLectureBtn;
+    private CardView AboutusBtn;
+    private Context context;
 
     public PersonalFragment() {
         // Required empty public constructor
@@ -64,6 +76,7 @@ public class PersonalFragment extends Fragment implements PersonalConstract.View
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        context = inflater.getContext();
         View view = inflater.inflate(R.layout.fragment_personal, container, false);
         UserNameText = view.findViewById(R.id.personal_username);
         IntroductionText = view.findViewById(R.id.personal_introduction);
@@ -72,6 +85,17 @@ public class PersonalFragment extends Fragment implements PersonalConstract.View
         PublishedLectureNumText = view.findViewById(R.id.personal_publishlecturenum);
         StudingLectureNumText = view.findViewById(R.id.personal_studylecturenum);
         CollectedLectureNumText = view.findViewById(R.id.personal_collectedlecturenum);
+
+        SettingBtn = view.findViewById(R.id.personal_setting_btn);
+        SettingBtn.setOnClickListener(this);
+        PublishedLectureBtn = view.findViewById(R.id.personal_btn_mylecture);
+        PublishedLectureBtn.setOnClickListener(this);
+        StudingLectureBtn = view.findViewById(R.id.personal_btn_mystudyinglecture);
+        StudingLectureBtn.setOnClickListener(this);
+        CollectedLectureBtn = view.findViewById(R.id.personal_btn_collect);
+        CollectedLectureBtn.setOnClickListener(this);
+        AboutusBtn = view.findViewById(R.id.personal_btn_aboutus);
+        AboutusBtn.setOnClickListener(this);
         return inflater.inflate(R.layout.fragment_personal, container, false);
     }
 
@@ -89,5 +113,30 @@ public class PersonalFragment extends Fragment implements PersonalConstract.View
         PublishedLectureNumText.setText(info.getPublishedLectureNum());
         StudingLectureNumText.setText(info.getStudingLectureNum());
         CollectedLectureNumText.setText(info.getCollectedLectureNum());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.personal_setting_btn: {
+                Intent intent = new Intent(context, DetailMessageActivity.class);
+                context.startActivity(intent);
+                break;
+            }
+            case R.id.personal_btn_mylecture:{
+
+                break;
+            }
+            case R.id.personal_btn_mystudyinglecture: {
+
+                break;
+            }
+            case R.id.personal_btn_collect: {
+                break;
+            }
+            case R.id.personal_btn_aboutus : {
+                break;
+            }
+        }
     }
 }
