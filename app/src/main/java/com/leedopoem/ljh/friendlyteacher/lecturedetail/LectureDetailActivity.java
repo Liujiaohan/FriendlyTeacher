a69ba780c8cf45f0e4af6f784474027f8828ba63
Liujiaohan <329598642@qq.com>
2017-11-05 19:23:52 +0800

    添加了课程详情

--- app/src/main/java/com/leedopoem/ljh/friendlyteacher/lecturedetail/LectureDetailActivity.java
+++ app/src/main/java/com/leedopoem/ljh/friendlyteacher/lecturedetail/LectureDetailActivity.java
@@ -11,39 +11,62 @@ import android.widget.Toast;
 import com.leedopoem.ljh.friendlyteacher.R;
 import com.leedopoem.ljh.friendlyteacher.data.entity.Lecture;
 
+import org.w3c.dom.Text;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LectureDetailActivity extends AppCompatActivity {
-    @BindView(R.id.tv_lecture_detail_name)
+
     TextView tvLectureName;
-    @BindView(R.id.tv_lecture_detail_teacher)
+
     TextView tvLectureTeacher;
-    @BindView(R.id.tv_lecture_detail_begin_time)
+
     TextView tvLectureBeginTime;
-    @BindView(R.id.tv_lecture_detail_class_introduction)
+
     TextView tvLectureClassTime;
-    @BindView(R.id.tv_lecture_detail_class_type)
+
     TextView tvLectureType;
-    @BindView(R.id.btn_join_lecture)
-    Button btnJoin;
+
+    TextView tvLectureIntroduction;
+
+    TextView tvLectureLocation;
+
+    Button btnJoin,btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture_detail);
-        ButterKnife.bind(this);
         Intent intent=getIntent();
         Lecture lecture= (Lecture) intent.getSerializableExtra("lecture");
+        tvLectureName= (TextView) findViewById(R.id.tv_lecture_detail_name);
+        tvLectureBeginTime= (TextView) findViewById(R.id.tv_lecture_detail_begin_time);
+        tvLectureType= (TextView) findViewById(R.id.tv_lecture_detail_class_type);
+        tvLectureClassTime= (TextView) findViewById(R.id.tv_lecture_detail_class_time);
+        tvLectureTeacher= (TextView) findViewById(R.id.tv_lecture_detail_teacher);
+        tvLectureIntroduction= (TextView) findViewById(R.id.tv_lecture_detail_class_introduction);
+        tvLectureLocation= (TextView) findViewById(R.id.tv_lecture_detail_location);
+        btnBack= (Button) findViewById(R.id.message_detail_back_btn);
+        btnJoin= (Button) findViewById(R.id.btn_join_lecture);
+
         tvLectureName.setText(lecture.getLectureName());
         tvLectureBeginTime.setText("2017-6-1~2017-9-1");
         tvLectureClassTime.setText(lecture.getClassTime());
         tvLectureTeacher.setText("杜恩博");
         tvLectureType.setText("编程");
+        tvLectureLocation.setText(lecture.getLocation());
+        tvLectureIntroduction.setText(lecture.getIntroduction());
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(LectureDetailActivity.this,"加入成功",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 finish();
             }
         });
